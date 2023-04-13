e9b4a3c18a67a2c3b3851b508ec83f0a7ea0423e
joshua tang tong hi <joshuatangtonghi@icloud.com>
2023-04-13 14:07:31 +0200

    pdf

---
+++ java/laverie/src/fr/esgi/poo/joshua/MainPasStatic.java
@@ -0,0 +1,15 @@
+package fr.esgi.poo.joshua;
+
+public class MainPasStatic {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    public boolean[] isWashingMachineUsed(){
+        boolean[] isWashingMachineUsed = new boolean[3];
+        return isWashingMachineUsed;
+    }
+    public void startLaundromat(){
+
+    }
+
+}
